89d53fff6db3203d15695c8a6dabc00f09e48357
Alex <a.mishanin@icl.kazan.ru>
2022-03-16 18:01:49 +0300

    new_setup_zfs

--- setup_zfs.sh
+++ setup_zfs.sh
@@ -11,8 +11,8 @@ yum install -y zfs
 modprobe zfs
 
 zpool create otus1 mirror /dev/sdb /dev/sdc
-zpool create otus1 mirror /dev/sdd /dev/sde
-zpool create otus1 mirror /dev/sdf /dev/sdg
-zpool create otus1 mirror /dev/sdh /dev/sdi
+zpool create otus2 mirror /dev/sdd /dev/sde
+zpool create otus3 mirror /dev/sdf /dev/sdg
+zpool create otus4 mirror /dev/sdh /dev/sdi
 
 
