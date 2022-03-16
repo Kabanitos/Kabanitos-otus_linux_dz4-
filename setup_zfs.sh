ef2e8f1f2295c5f8c6d9fc488601f7ad9d91c1a9
Kabanitos <89770490+Kabanitos@users.noreply.github.com>
2022-03-17 00:25:10 +0300

    Update setup_zfs.sh

--- setup_zfs.sh
+++ setup_zfs.sh
@@ -2,7 +2,7 @@
 
 yum install -y  https://zfsonlinux.org/epel/zfs-release.el7_9.noarch.rpm 
 rpm --import /etc/pki/rpm-gpg/RPM-GPG-KEY-zfsonlinux
-yum install -y epel-release kernel-devel zfs
+yum install -y epel-release kernel-devel zfs wget
 
 yum-config-manager --enable zfs-kmod
 yum-config-manager --disable zfs
